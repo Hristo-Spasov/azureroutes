3952e1af699b22259b5c2fd27d8cb9995c05835b
Trickkyyy <vandark5@gmail.com>
2023-12-14 22:23:41 +0200

    formatting the dropdown menu

--- src/components/FlightsListDetailedData/FlightsListDetailedData.tsx
+++ src/components/FlightsListDetailedData/FlightsListDetailedData.tsx
@@ -1,6 +1,7 @@
 import style from "./FlightsListDetailedData.module.scss";
 import { useState } from "react";
 import DownArrow from "../../assets/down-arrow-svgrepo-com.svg?react";
+import InFlight from "../../assets/airplane-in-flight-thin.svg?react";
 
 type FlightProps = {
   icao: string;
@@ -39,23 +40,53 @@ const FlightsListDetailedData = (el: FlightProps) => {
         <li className={style.detailed_information}>
           <div className={style.status}>
             <span>Date | Timezone</span>
+            <div className={style.flight_number}>
+              <span>Flight</span>
+              <span>W9432</span>
+            </div>
             <span>Status</span>
           </div>
-          <span>Departure</span>
-          <span>delay</span>
+
+          <span>Plovdiv,PDV</span>
           <div className={style.dropdown_departure}>
-            <span>airport,iata</span>
-            <span>scheduled</span>
-            <span>estimated</span>
-            <span>actual</span>
+            <div className={style.departure_sub_class}>
+              <span>scheduled</span>
+              <span>2023/12/12 | 13:00 </span>
+            </div>
+            <div className={style.departure_sub_class}>
+              <span>delay</span>
+              <span>60 min</span>
+            </div>
+            <div className={style.departure_sub_class}>
+              <span>estimated</span>
+              <span>2023/12/12 | 14:00 </span>
+            </div>
+            <div className={style.departure_sub_class}>
+              <span>actual</span>
+              <span>2023/12/12 | 14:00 </span>
+            </div>
           </div>
-          <span>Arrival</span>
-          <span>delay</span>
+
+          <InFlight width={50} height={50} />
+
+          <span>Sofia,SOF</span>
           <div className={style.dropdown_arrival}>
-            <span>airport,iata</span>
-            <span>scheduled</span>
-            <span>estimated</span>
-            <span>actual</span>
+            <div className={style.arrival_sub_class}>
+              <span>scheduled</span>
+              <span>2023/12/12 | 13:00 </span>
+            </div>
+            <div className={style.arrival_sub_class}>
+              <span>delay</span>
+              <span>60 min</span>
+            </div>
+            <div className={style.arrival_sub_class}>
+              <span>estimated</span>
+              <span>2023/12/12 | 13:00 </span>
+            </div>
+            <div className={style.arrival_sub_class}>
+              <span>actual</span>
+              <span>2023/12/12 | 14:00 </span>
+            </div>
           </div>
         </li>
       )}
