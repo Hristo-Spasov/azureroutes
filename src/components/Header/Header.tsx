cf00bf773d7f2be0214893986ef164bf3340b20b
Trickkyyy <vandark5@gmail.com>
2024-02-19 21:22:20 +0200

    adding media queries for small desktops

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import style from "./Header.module.scss";
-import logo from "../../assets/trickkyyy_logovector_imageairplanewhite_and_blue_colors_20c5c259-2a25-41b0-bd0d-5dbb3e6c82e5.png";
+import logo from "../../public/logo.png";
 
 const Header = (): JSX.Element => {
   return (
