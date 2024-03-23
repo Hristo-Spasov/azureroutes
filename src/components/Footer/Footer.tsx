130dc1fcb5465d031e3ad89a5d1fbeedb6970252
Trickkyyy <vandark5@gmail.com>
2024-03-23 17:56:16 +0200

    styling the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,28 +2,52 @@ import styles from "./Footer.module.scss";
 import Github from "../../assets/socials/github.svg?react";
 import Facebook from "../../assets/socials/facebook.svg?react";
 import Instagram from "../../assets/socials/instagram.svg?react";
+import dayjs from "dayjs";
+import { Link } from "react-router-dom";
+import useResize from "../../hooks/useResize";
 const Footer = () => {
+  const mobile = useResize(600);
   return (
     <section className={styles.footer_container}>
-      <span className={styles.sitemap}>
-        <h3>
-          <strong>Sitemap</strong>
-        </h3>
-        <ul>
-          <li>Home</li>
-          <li>Tickets</li>
-          <li>Hotels</li>
-          <li>Rent A Car</li>
-        </ul>
-      </span>
-      <span className={styles.socials}>
-        <h3>Contact Us</h3>
-        <span className={styles.icon_container}>
-          <Github width={30} height={30} />
-          <Facebook width={30} height={30} />
-          <Instagram width={30} height={30} />
+      <div className={styles.content_wrapper}>
+        <span className={styles.socials}>
+          <a
+            rel="noopener noreferrer"
+            href="https://github.com/Hristo-Spasov/azureroutes"
+            target="_blank"
+          >
+            <Github width={40} height={40} />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="http://facebook.com"
+            target="_blank"
+          >
+            <Facebook width={40} height={40} />
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="http://instagram.com"
+            target="_blank"
+          >
+            <Instagram width={40} height={40} />
+          </a>
         </span>
-      </span>
+        <span className={styles.footer_nav_container}>
+          <nav className={styles.footer_nav}>
+            <Link to="/">Home</Link>
+            {mobile && <hr />}
+            <Link to="/">Tickets</Link>
+            {mobile && <hr />}
+            <Link to="/">Hotels</Link>
+            {mobile && <hr />}
+            <Link to="/">Rent A Car</Link>
+            {mobile && <hr />}
+          </nav>
+        </span>
+      </div>
+
+      <p>{`Copyright © 2024 - ${dayjs().year()} Azure Routes. All rights reserved.`}</p>
     </section>
   );
 };
