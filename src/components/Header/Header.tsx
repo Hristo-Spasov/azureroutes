852a18a5284817243bc4168c0831020bf86ba3b7
Trickkyyy <vandark5@gmail.com>
2024-03-23 21:21:37 +0200

    styling the Error page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import style from "./Header.module.scss";
 import logo from "../../public/logo.png";
+import name from "../../assets/azure routes.png";
 
 const Header = (): JSX.Element => {
   return (
     <header className={style.header}>
-      <img src={logo} alt="skywise_logo" />
-      <h1>
+      <img src={logo} alt="skywise_logo" className={style.logo} />
+      {/* <h1>
         Azure
         <br />
         Routes
-      </h1>
+      </h1> */}
+      <img src={name} alt="Name" className={style.name} />
       <span></span> {/* TODO navbar in the future */}
     </header>
   );
--- src/pages/Error/ErrorPage.tsx
+++ src/pages/Error/ErrorPage.tsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./ErrorPage.module.scss";
-import Header from "../../components/Header/Header";
+// import Header from "../../components/Header/Header";
 
 const ErrorPage = () => {
   return (
     <>
-      <Header />
       <div className={styles.container}>
-        <h1 className={styles.code}>404</h1>
+        {/* <h1 className={styles.code}>404</h1>
         <h2>Oops,</h2>
-        <p>Are you lost?</p>
-        <Link to="/">Get back Home</Link>
+        <p>Are you lost?</p> */}
+        <Link to="/">Get back Home?</Link>
       </div>
     </>
   );
