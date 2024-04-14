677b8817eb7481715ebec0a85cd0be650168b613
Trickkyyy <vandark5@gmail.com>
2024-04-14 22:59:19 +0300

    adding types for news retured data

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -3,9 +3,10 @@ import style from "./News.module.scss";
 import { useEffect, useState } from "react";
 // import NewsModal from "./NewsModal";
 import fetchNews from "../../utils/fetchNews";
+import { NewsDataType, NewsResponse } from "../../types/news_types";
 
 const News = () => {
-  const [news, setNews] = useState<any>();
+  const [news, setNews] = useState<NewsResponse<NewsDataType>>();
   // const [isOpen, setIsOpen] = useState(false);
   // const [selectedArticle, setSelectedArticle] = useState(null);
 
@@ -22,6 +23,7 @@ const News = () => {
     }
   }, [cachedNews]);
 
+  //Console log
   useEffect(() => {
     console.log(news);
   }, [news]);
@@ -34,35 +36,33 @@ const News = () => {
 
   return (
     <section className={style.news_section}>
-      <h2>Latest News</h2>
+      <h2>Latest Travel News</h2>
       <button onClick={() => refetch()}>click</button>
 
       <div className={style.news_container}>
-        {news?.results.map((article: any) => (
-          <>
-            <div className={style.news_card} key={article.article_id}>
-              <img src={article.image_url} alt="" />
-              <h3>{article.title}</h3>
-              <p>{`${
-                article.description != null
-                  ? article.description.slice(0, 250)
-                  : ""
-              }...`}</p>
-              <span className={style.lower_card}>
-                {/* <button type="button" onClick={() => handleModal(article)}>
+        {news?.results.map((article) => (
+          <div className={style.news_card} key={article.article_id}>
+            <img src={article.image_url} alt="" />
+            <h3>{article.title}</h3>
+            <p>{`${
+              article.description != null
+                ? article.description.slice(0, 250)
+                : ""
+            }...`}</p>
+            <span className={style.lower_card}>
+              {/* <button type="button" onClick={() => handleModal(article)}>
                   Read more...
                 </button> */}
-                <a
-                  href={`${article.link}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Read more...
-                </a>
-                <p>{article.pubDate}</p>
-              </span>
-            </div>
-          </>
+              <a
+                href={`${article.link}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Read more...
+              </a>
+              <p>{article.pubDate}</p>
+            </span>
+          </div>
         ))}
       </div>
       {/* <NewsModal hasCloseBtn={true} isOpen={isOpen} onClose={handleModal}>
--- src/utils/fetchNews.ts
+++ src/utils/fetchNews.ts
@@ -1,13 +1,11 @@
+import { NewsDataType, NewsResponse } from "../types/news_types";
 import fetchData from "./fetchData";
 
-interface ApiResponse<T> {
-  data: T[];
-}
 const BASE_URL = "https://newsdata.io/api/1/";
 const API_KEY = "pub_41527d6d0de206640fc1f63c5573def105003";
 
 const fetchNews = async () => {
-  const data = await fetchData<ApiResponse<any>>({
+  const data = await fetchData<NewsResponse<NewsDataType>>({
     url: `${BASE_URL}news?apikey=${API_KEY}&language=en&category=tourism`,
   });
   return data;
