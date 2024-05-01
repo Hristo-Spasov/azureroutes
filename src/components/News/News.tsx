ef30e8ba67f2aa7bdab93bdc194e58327518739b
Trickkyyy <vandark5@gmail.com>
2024-05-01 19:33:42 +0300

    implementing the fetching logic for the news api through the server

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,13 +1,15 @@
 import { useQuery } from "react-query";
 import style from "./News.module.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 // import NewsModal from "./NewsModal";
 import fetchNews from "../../utils/fetchNews";
 import { NewsDataType, NewsResponse } from "../../types/news_types";
+import fallback_photo from "../../assets/fallback_img.jpg";
+import Spinner from "../Spinner/Spinner";
 
 const News = () => {
   const [news, setNews] = useState<NewsResponse<NewsDataType>>();
-  // const observeRef = useRef(null);
+  const observeRef = useRef(null);
   // const [isOpen, setIsOpen] = useState(false);
   // const [selectedArticle, setSelectedArticle] = useState(null);
 
@@ -15,30 +17,32 @@ const News = () => {
     queryKey: ["news"],
     queryFn: () => fetchNews(),
     enabled: false,
+    cacheTime: 0,
+    staleTime: 0,
     onSuccess: (data) => setNews(data),
   });
 
   //! Intersection Observer provide the ref to the upmost div
-  // const options = {
-  //   root: null,
-  //   rootMargin: "0px",
-  //   threshold: 0.5,
-  // };
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
 
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver((entries) => {
-  //     entries.forEach((entry) => {
-  //       if (entry.isIntersecting) {
-  //         refetch(); // Refetch when element comes into view
-  //       }
-  //     });
-  //   }, options);
-  //   if (observeRef.current) observer.observe(observeRef.current);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          refetch(); // Refetch when element comes into view
+        }
+      });
+    }, options);
+    if (observeRef.current) observer.observe(observeRef.current);
 
-  //   return () => {
-  //     if (observeRef.current) observer.unobserve(observeRef.current);
-  //   };
-  // }, [observeRef, refetch]);
+    return () => {
+      if (observeRef.current) observer.unobserve(observeRef.current);
+    };
+  }, [observeRef, refetch]);
 
   useEffect(() => {
     if (cachedNews) {
@@ -46,10 +50,10 @@ const News = () => {
     }
   }, [cachedNews]);
 
-  //Console log
-  useEffect(() => {
-    console.log(news);
-  }, [news]);
+  //! Console log
+  // useEffect(() => {
+  //   console.log(news);
+  // }, [news]);
 
   // const handleModal = (article: any) => {
   //   setIsOpen(!isOpen);
@@ -58,36 +62,45 @@ const News = () => {
   // };
 
   return (
-    <section className={style.news_section}>
+    <section className={style.news_section} ref={observeRef}>
       <h2>Latest Travel News</h2>
-      <button onClick={() => refetch()}>click</button>
-
-      <div className={style.news_container}>
-        {news?.results.map((article) => (
-          <div className={style.news_card} key={article.article_id}>
-            <img src={article.image_url} alt="" />
-            <h3>{article.title}</h3>
-            <p>{`${
-              article.description != null
-                ? article.description.slice(0, 250) + "..."
-                : ""
-            }`}</p>
-            <span className={style.lower_card}>
-              {/* <button type="button" onClick={() => handleModal(article)}>
+      {/* Button for fetch testing */}
+      {/* <button onClick={() => refetch()}>click</button>*/}
+      {!news ? (
+        <Spinner />
+      ) : (
+        <div className={style.news_container}>
+          {news?.results.map((article) => (
+            <div className={style.news_card} key={article.article_id}>
+              <img
+                src={!article.image_url ? fallback_photo : article.image_url}
+                alt={article.title}
+                onError={(e) => (e.currentTarget.src = fallback_photo)}
+              />
+              <h3>{article.title}</h3>
+              <p>{`${
+                article.description != null
+                  ? article.description.slice(0, 250) + "..."
+                  : ""
+              }`}</p>
+              <span className={style.lower_card}>
+                {/* <button type="button" onClick={() => handleModal(article)}>
                   Read more...
                 </button> */}
-              <a
-                href={`${article.link}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Read more...
-              </a>
-              <p>{article.pubDate}</p>
-            </span>
-          </div>
-        ))}
-      </div>
+                <a
+                  href={`${article.link}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read more...
+                </a>
+                <p>{article.pubDate}</p>
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
+
       {/* <NewsModal hasCloseBtn={true} isOpen={isOpen} onClose={handleModal}>
         <img src={selectedArticle?.image_url} alt="" />
         <h3>{selectedArticle?.title}</h3>
--- src/utils/fetchNews.ts
+++ src/utils/fetchNews.ts
@@ -1,12 +1,9 @@
 import { NewsDataType, NewsResponse } from "../types/news_types";
 import fetchData from "./fetchData";
 
-const BASE_URL = "https://newsdata.io/api/1/";
-const API_KEY = "pub_421527d5eb357887bf3e984cbb743d5898988";
-
 const fetchNews = async () => {
   const data = await fetchData<NewsResponse<NewsDataType>>({
-    url: `${BASE_URL}news?apikey=${API_KEY}&language=en&category=tourism`,
+    url: `http://localhost:3001/api/v1/news/`,
   });
   return data;
 };
