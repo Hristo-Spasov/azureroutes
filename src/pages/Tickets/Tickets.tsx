71c0dc72feb151a3241db2a76f13c95d2ffd0809
Trickkyyy <vandark5@gmail.com>
2024-05-12 15:43:51 +0300

    widget preview

--- src/pages/Tickets/Tickets.tsx
+++ src/pages/Tickets/Tickets.tsx
@@ -9,8 +9,6 @@ const Tickets = () => {
           async
           src="https://tp.media/content?campaign_id=121&promo_id=4132&color_border=%232681ff&color_button_text=%23ffffff&color_button=%232681ff&plain=true&border_radius=0&powered_by=true&default_origin=London&curr=EUR&locale=en&shmarker=546163&trs=320944"
           charSet="utf-8"
-          onLoad={() => console.log("Script loaded successfully")}
-          onError={() => console.error("Error loading script")}
         ></script>
       </Helmet>
     </div>
