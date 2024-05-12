856bc6172260d49f8d59fadf72a1e6ca58d4b764
Trickkyyy <vandark5@gmail.com>
2024-05-12 15:48:51 +0300

    widget preview

--- src/pages/Tickets/Tickets.tsx
+++ src/pages/Tickets/Tickets.tsx
@@ -7,7 +7,7 @@ const Tickets = () => {
       <Helmet>
         <script
           async
-          src="https://tp.media/content?campaign_id=121&promo_id=4132&color_border=%232681ff&color_button_text=%23ffffff&color_button=%232681ff&plain=true&border_radius=0&powered_by=true&default_origin=London&curr=EUR&locale=en&shmarker=546163&trs=320944"
+          src="https://tp.media/content?campaign_id=121&promo_id=4132&color_border=%232681ff&color_button_text=%23ffffff&color_button=%232681ff&plain=true&border_radius=0&powered_by=true&default_origin=London&curr=EUR&locale=en&shmarker=546163&trs=320952"
           charSet="utf-8"
         ></script>
       </Helmet>
