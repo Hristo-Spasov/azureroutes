c41668e9b711d05343b87e08a73d7eb77552079a
Trickkyyy <vandark5@gmail.com>
2024-06-21 20:58:14 +0300

    fixing a bug related to unwanted weather fetches

--- src/components/FlightList/FlightList.tsx
+++ src/components/FlightList/FlightList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import FlightGeneralInfo from "../FlightGeneralInfo/FlightGeneralInfo";
 import { FetchContext } from "../../context/fetch-context";
 import { ClockContext } from "../../context/clock-context";
